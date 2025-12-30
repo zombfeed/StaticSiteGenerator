978a85d7150e3865e205809a6b19e572bcce0bc4
zombfeed <rdgammad@gmail.com>
2025-12-30 13:26:34 -0800

    CH4:L2, add block types and unittests

--- src/markdown_blocks.py
+++ src/markdown_blocks.py
@@ -1,2 +1,60 @@
+from enum import Enum
+
+
+class BlockType(Enum):
+    PARAGRAPH = 0
+    HEADING = 1
+    CODE = 2
+    QUOTE = 3
+    UNORDERED_LIST = 4
+    ORDERED_LIST = 5
+
+
+def block_to_block_type(block):
+    block_type = BlockType.PARAGRAPH
+    if block.startswith("######"):
+        block_type = BlockType.HEADING
+    elif block.startswith("```") and block.endswith("```"):
+        block_type = BlockType.CODE
+    else:
+        is_incrementing = 1
+        previous = BlockType.PARAGRAPH
+        lines = block.split("\n")
+        for i in range(len(lines)):
+            if lines[i].startswith(">"):
+                if i == 0:
+                    previous = BlockType.QUOTE
+
+                block_type = (
+                    BlockType.QUOTE
+                    if previous != BlockType.PARAGRAPH
+                    else BlockType.PARAGRAPH
+                )
+            elif lines[i].startswith("- "):
+                if i == 0:
+                    previous = BlockType.UNORDERED_LIST
+                block_type = (
+                    BlockType.UNORDERED_LIST
+                    if previous != BlockType.PARAGRAPH
+                    else BlockType.PARAGRAPH
+                )
+            elif lines[i][0].isdigit() and int(lines[i][0]) == is_incrementing:
+                if lines[i].startswith(f"{lines[i][0]}. "):
+                    is_incrementing += 1
+                    block_type = BlockType.ORDERED_LIST
+                else:
+                    block_type = BlockType.PARAGRAPH
+            else:
+                block_type = BlockType.PARAGRAPH
+    return block_type
+
+
 def markdown_to_blocks(markdown):
     return [block.strip() for block in markdown.split("\n\n")]
+
+
+if __name__ == "__main__":
+    md = "```this is a code block```"
+    md2 = "```this is a code block\nwith multiple lines```"
+    print(block_to_block_type(md))
+    print(block_to_block_type(md2))
--- src/test_markdown_blocks.py
+++ src/test_markdown_blocks.py
@@ -1,5 +1,6 @@
+from typing import MutableMapping
 import unittest
-from markdown_blocks import markdown_to_blocks
+from markdown_blocks import BlockType, markdown_to_blocks, block_to_block_type
 
 
 class TestMarkddownToBlocks(unittest.TestCase):
@@ -36,6 +37,92 @@ this doesn't
 """
         blocks = markdown_to_blocks(md)
         self.assertListEqual(blocks, ["THIS HAS WHITESPACE\nthis doesn't\n-this does"])
+
+
+class TestBlockToBlockType(unittest.TestCase):
+    def test_block_is_paragraph(self):
+        block = "this is a paragraph"
+        self.assertEqual(block_to_block_type(block), BlockType.PARAGRAPH)
+
+    def test_block_is_heading(self):
+        block = "###### this is a heading"
+        self.assertEqual(block_to_block_type(block), BlockType.HEADING)
+
+    def test_block_is_not_heading(self):
+        block = "#### this is not a heading"
+        self.assertNotEqual(block_to_block_type(block), BlockType.HEADING)
+
+    def test_block_is_code(self):
+        block = "```this is a code block```"
+        multiline_block = "```this is a code block\nwith multiple lines```"
+        self.assertEqual(block_to_block_type(block), BlockType.CODE)
+        self.assertEqual(block_to_block_type(multiline_block), BlockType.CODE)
+
+    def test_block_is_not_code(self):
+        block = "```this is not code"
+        multiline_block = "```this is not code\nbecause it doesnt end in backquotes"
+        multiline_block2 = (
+            "this is not code\nbecause it doesnt start with backquotes```"
+        )
+        self.assertNotEqual(block_to_block_type(block), BlockType.CODE)
+        self.assertNotEqual(block_to_block_type(multiline_block), BlockType.CODE)
+        self.assertNotEqual(block_to_block_type(multiline_block2), BlockType.CODE)
+
+    def test_block_is_quote(self):
+        block = ">this is a quote"
+        multiline_block = ">this is a quote\n> this is also a quote"
+        self.assertEqual(block_to_block_type(block), BlockType.QUOTE)
+        self.assertEqual(block_to_block_type(multiline_block), BlockType.QUOTE)
+
+    def test_block_is_not_quote(self):
+        block = "this is not a quote"
+        multiline_block = ">this is not a block\nbecause not every line starts with a >"
+        self.assertNotEqual(block_to_block_type(block), BlockType.QUOTE)
+        self.assertNotEqual(block_to_block_type(multiline_block), BlockType.QUOTE)
+
+    def test_block_is_unordered(self):
+        block = "- this is unordered"
+        multiline_block = "- this is unordered\n- this is the second line"
+        self.assertEqual(block_to_block_type(block), BlockType.UNORDERED_LIST)
+        self.assertEqual(block_to_block_type(multiline_block), BlockType.UNORDERED_LIST)
+
+    def test_block_is_not_unordered(self):
+        block = "-this is not unordered"
+        multiline_block = (
+            "- this is not unordered\n-because not every line starts with '- '"
+        )
+        multiline_block2 = (
+            "-this is not unordered\n- because not every line starts with '- '"
+        )
+        self.assertNotEqual(block_to_block_type(block), BlockType.UNORDERED_LIST)
+        self.assertNotEqual(
+            block_to_block_type(multiline_block), BlockType.UNORDERED_LIST
+        )
+        self.assertNotEqual(
+            block_to_block_type(multiline_block2), BlockType.UNORDERED_LIST
+        )
+
+    def test_block_is_ordered(self):
+        block = "1. This is ordered"
+        multiline_block = (
+            "1. This is ordered\n2. This is the second\n3. This is the third"
+        )
+        self.assertEqual(block_to_block_type(block), BlockType.ORDERED_LIST)
+        self.assertEqual(block_to_block_type(multiline_block), BlockType.ORDERED_LIST)
+
+    def test_block_is_not_ordered(self):
+        block = "1.This is not ordered"
+        block2 = "2. This is not ordered"
+        multiline_block = "1. This is not ordered\n2.This is not ordered"
+        mutliline_block2 = "1. This is not ordered\n3. This is not ordered"
+        self.assertNotEqual(block_to_block_type(block), BlockType.ORDERED_LIST)
+        self.assertNotEqual(block_to_block_type(block2), BlockType.ORDERED_LIST)
+        self.assertNotEqual(
+            block_to_block_type(multiline_block), BlockType.ORDERED_LIST
+        )
+        self.assertNotEqual(
+            block_to_block_type(mutliline_block2), BlockType.ORDERED_LIST
+        )
 
 
 if __name__ == "__main__":
